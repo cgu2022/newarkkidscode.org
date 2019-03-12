9233aaee2b2955d1634cf545e4ee277c31af8648
Chris Gu <cgu2022@pingry.org>
2019-03-11 22:10:06 -0400

    Updated Navbar + added sum cool stuff onto it. Added more content for AboutUs2 page

--- Chris - About Us/sc.js
+++ Chris - About Us/sc.js
@@ -1,4 +1,19 @@
-﻿
+﻿/*Navbar Stuff*/
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        var h = -$('#navbar').height()-20;
+        console.log(h);
+        document.getElementById("navbar").style.top = h.toString() + "px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+/*Other Stuff*/
 function Utils() {
 
 }
