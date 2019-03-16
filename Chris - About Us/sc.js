96c5650cc6362f59cfbf6eb9803c807e71ebbf4f
Chris Gu <cgu2022@pingry.org>
2019-03-16 16:48:38 -0400

    Revert "Added Gavin Song's thing"
    This reverts commit 46d5af773dc207e709a8c2d08cd340b665d2506f.

--- Chris - About Us/sc.js
+++ Chris - About Us/sc.js
@@ -20,11 +20,11 @@ function Utils() {
 
 Utils.prototype = {
     constructor: Utils,
-    isElementInView: function (element, fullyInView) {
+    isElementInView: function (element, fullyInView, offseter) {
         var pageTop = $(window).scrollTop();
         var pageBottom = pageTop + $(window).height();
         var elementTop = $(element).offset().top;
-        var elementBottom = elementTop + $(element).height();
+        var elementBottom = elementTop + $(element).height() + offseter;
 
         console.clear()
         console.log(pageTop)
@@ -32,11 +32,15 @@ Utils.prototype = {
         console.log(elementTop)
         console.log(elementBottom)
 
-        if (fullyInView === true) {
+        if (fullyInView === 2) { //The moment it sees the element
             return ((pageTop < elementTop) && (pageBottom > elementBottom));
         }
-          else {
-        return((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        else if (fullyInView === 1) //Just At least the full element
+        {
+            return (pageBottom > elementBottom);
+        }
+        else { //After
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
         }
     }
 };
@@ -72,7 +76,7 @@ function autoType(elementClass, typingSpeed) {
 
 var von = false;
 $(window).scroll(function () {
-    isElementInView = Utils.isElementInView($('#three'), false);
+    isElementInView = Utils.isElementInView($('.text-js'), 1, 15);
     if (isElementInView && !von) {
         autoType(".type-js", 100);
         von = true;
